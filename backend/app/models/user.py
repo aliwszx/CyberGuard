3eb4436b17b9c88838489bfd603cacfc0f66d488
Ali Baghirzada <bagirzadeh295@gmail.com>
2026-06-15 19:31:25 +0400

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
-from app.database.base import Base
+from backend.app.database.base import Base
 
 class User(Base):
     __tablename__ = "users"
